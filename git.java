7f8df749fbf2882a779f837a2f134f4867a0ec10
Nolan Krieger <nolankrieger9@gmail.com>
2025-04-16 19:44:41 -0400

    added methods class

--- git.java
+++ git.java
@@ -1,8 +1,12 @@
 import github.tools.client.GitHubApiClient;
 import github.tools.responseObjects.*;
 import git.tools.client.GitSubprocessClient;
+import java.awt.*;
+import javax.swing.*;
+
 public class git{
+	
     public static void main(String[] args){
-        
+       
     }
 }
