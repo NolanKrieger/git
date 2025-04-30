27014e539fdf38dc196ae661d7d9221018bf14ef
jfk004 <kalangefahim01@gmail.com>
2025-04-30 16:08:25 -0400

    Removed unused methods and imports

--- git.java
+++ git.java
@@ -1,15 +1,5 @@
-// import github.tools.client.GitHubApiClient;
-// import github.tools.responseObjects.*;
-// import git.tools.client.GitSubprocessClient;
-// import java.awt.*;
-// import java.io.FileWriter;
-// import java.io.IOException;
-
-// import javax.management.RuntimeErrorException;
 import javax.swing.SwingUtilities;
-
 public class git{
- 
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
             new GitUI().setVisible(true);
